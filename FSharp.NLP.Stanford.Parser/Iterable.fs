af4c30b39b069fdf5d4ce73c78752827395fda69
jliettehk <jliettehk@users.noreply.github.com>
2013-09-20 05:52:28 +0200

    Update Iterable.fs
    Hello Sergey,
    I have put an example that shows how you can reproduce the problem with the ghost array :
    - take your existing module ParserDemo2 (file ParserDemo2.fs)
    - in line 22, 'sentences' is defined
    - suppose that you insert the following code before the 'for loop' of line 39 :
       let length = Seq.length sentences
    - it will execute giving you the right length
    - if you add another call to Seq like : let length2 = Seq.length sentences 
    - then length2 will be 0 <==== that's the PROBLEM
    - thereafter the 'for loop' starting in line 39 will not execute properly (no sentence in sentences)
    
    Have a good day,
    jliettehk

--- FSharp.NLP.Stanford.Parser/Iterable.fs
+++ FSharp.NLP.Stanford.Parser/Iterable.fs
@@ -7,6 +7,17 @@ module Iterable =
     // 2 changes proposed in toSeq function :
     // 1- yield moved after if x.hasNext()
     // 2- force a copy (as proposed by Tomasp on SO : http://stackoverflow.com/questions/6605253/wrapping-a-mutable-collection-with-a-sequence
+    // 
+    // Reason : runtime exception occured when playing with ParserDemo2 calling Iterable.toSeq
+    // Example :
+    // - take existing module ParserDemo2 (file ParserDemo2.fs in samples directory)
+    // - in line 22, 'sentences' is defined
+    // - suppose that you insert the following code before the 'for loop' of line 39 :
+         let length = Seq.length sentences  // or any Seq function
+    // - it will execute giving you the right length
+    // - if you add another call to Seq like : let length2 = Seq.length sentences 
+    // - then length2 will be 0 <==== that's the PROBLEM
+    // - thereafter the 'for loop' starting in line 39 will not execute properly (no sentence in sentences)
     let toSeq (iter:Iterable) =
         let rec loop (x:Iterator) = 
            seq { 
